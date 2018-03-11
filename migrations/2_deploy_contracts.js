28e39b300d1aa642df01ae69de89629b3032feca
thesved <2893181+thesved@users.noreply.github.com>
2018-03-11 23:46:20 +0100

    New migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -8,8 +8,14 @@ var EthealController = artifacts.require("EthealController");
 var RefundVault = artifacts.require("RefundVault");
 var Hodler = artifacts.require("Hodler");
 var TokenVesting = artifacts.require("TokenVesting");
+var EthealDeposit = artifacts.require("EthealDeposit");
+var EthealWhitelist = artifacts.require("EthealWhitelist");
+var EthealPromoTokenController = artifacts.require("EthealPromoTokenController");
+var EthealPromoToken = artifacts.require("EthealPromoToken");
+var ECRecovery = artifacts.require("ECRecovery");
+var ED,EN,EW;
 
-var dateStart = Math.floor(new Date().getTime()/1000)+5*60*60*24; // starts in 5 days
+var dateStart = Math.floor(new Date().getTime()/1000)+30*60; // starts in 30 minutes
 var dateEnd = dateStart + 10*24*60*60; // lasts 10 days
 
 module.exports = function(deployer) {
@@ -18,7 +24,13 @@ module.exports = function(deployer) {
 		return deployer.deploy(SafeMath);
 	}).then(function(){
 		// link SafeMath
-		return deployer.link(SafeMath, [PreSale, NormalSale, RefundVault, EthealController, Hodler, TokenVesting]);
+		return deployer.link(SafeMath, [PreSale, NormalSale, RefundVault, EthealController, Hodler, TokenVesting, EthealDeposit, EthealWhitelist, EthealPromoTokenController]);
+	}).then(function(){
+		// deploy SafeMath first
+		return deployer.deploy(ECRecovery);
+	}).then(function(){
+		// link SafeMath
+		return deployer.link(ECRecovery, [EthealWhitelist]);
 	}).then(function(){
 		// then Wallet
 		return deployer.deploy(Wallet,[web3.eth.accounts[0],web3.eth.accounts[1],web3.eth.accounts[2]],2);
@@ -35,13 +47,40 @@ module.exports = function(deployer) {
 		// set Token for Crowdsale
 		return (EthealController.at(EthealController.address)).setEthealToken(EthealToken.address,0);
 	}).then(function(){
-		// then PreSale
-		return deployer.deploy(PreSale,EthealController.address,dateStart,dateEnd,web3.toWei(0.1, "ether"),1000,web3.toWei(10, "ether"),web3.toWei(50, "ether"),120*60*60,web3.toWei(100, "ether"),web3.toWei(100, "gwei"),80,Wallet.address);
+		// promo controller
+		return deployer.deploy(EthealPromoTokenController);
 	}).then(function(){
-		// then NormalSale
-		return deployer.deploy(NormalSale,EthealController.address,dateStart,dateEnd,web3.toWei(0.1, "ether"),1000,web3.toWei(50, "ether"),120*60*60,web3.toWei(100, "ether"),web3.toWei(100, "gwei"),80,Wallet.address);
+		// promo token
+		return deployer.deploy(EthealPromoToken,EthealPromoTokenController.address,MiniMeTokenFactory.address);
+	}).then(function(){
+		// whitelist
+		return deployer.deploy(EthealWhitelist,web3.eth.accounts[4]);
+	}).then(function(){
+		// then Normal Sale
+		return deployer.deploy(NormalSale,EthealController.address,dateStart,dateEnd,web3.toWei(0.1, "ether"),1000,web3.toWei(10, "ether"),120*60*60,web3.toWei(50, "ether"),Wallet.address);
 	}).then(function(){
 		// set crowdsale
-		return (EthealController.at(EthealController.address)).setCrowdsaleTransfer(PreSale.address,web3.toBigNumber(web3.toWei(100,"ether")).mul(1000));
-	});	
+		return (EthealController.at(EthealController.address)).setCrowdsaleTransfer(NormalSale.address,web3.toBigNumber(web3.toWei(50,"ether")).mul(1000));
+	}).then(function(){
+		// set promo token
+		return (NormalSale.at(NormalSale.address)).setPromoTokenController(EthealPromoTokenController.address);
+	}).then(function(){
+		// set whitelist
+		return (NormalSale.at(NormalSale.address)).setWhitelist(EthealWhitelist.address, web3.toWei(1, "ether"));
+	}).then(function(){
+		// promo controller set sale addr
+		return (EthealPromoTokenController.at(EthealPromoTokenController.address)).setCrowdsale(NormalSale.address);
+	}).then(function(){
+		// promo controller set promo token
+		return (EthealPromoTokenController.at(EthealPromoTokenController.address)).setPromoToken(EthealPromoToken.address);
+	}).then(function(){
+		// etheal deposit
+		return deployer.deploy(EthealDeposit,NormalSale.address,EthealWhitelist.address);
+	}).then(function(){
+		// set deposit
+		return (NormalSale.at(NormalSale.address)).setDeposit(EthealDeposit.address);
+	})/*.then(function(){
+		// contribute
+		return EthealDeposit.at(EthealDeposit.address).deposit(web3.eth.accounts[1],"0xa36ceb1eb4acda877cbedf5ffa18d791944739d4a4e8d38bdbe9956af9bbca81693cef62d2f5aee6153bda31e2ad3b2d5061a8f26177042fca55bc917ba29b9400",{value:web3.toWei(0.1,"ether")});
+	})*/;	
 };
